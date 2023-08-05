33949114b902755019f95d2db55512d448277487
Md Hasib Askari <31061360+Md-Hasib-Askari@users.noreply.github.com>
2023-08-05 12:28:38 +0600

    Delete q3.js

--- q3.js
+++
@@ -1,9 +0,0 @@
-function multiplicationTableGenerator(value) {
-    console.log("Generate Multiplication Table for: " + value);
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${i} x ${value} = ${i*value}`);
-    }
-
-}
-
-multiplicationTableGenerator(5);

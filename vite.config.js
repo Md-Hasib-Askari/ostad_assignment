ac37f673332b586da6957b2e906b113609c57450
Md Hasib Askari <mdhsbaskari@gmail.com>
2023-11-13 21:50:18 +0600

    Module 14 Assignment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ostad_assigment/',
+  base: '/ostad_assignment/',
   plugins: [react()],
 })

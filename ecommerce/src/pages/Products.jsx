c80400da5daa82abcf5996cee260bc977b3d6774
MDHASIBASKARI <mdhsbaskari@gmail.com>
2024-01-02 16:58:59 +0600

    Init module 19

---
+++ ecommerce/src/pages/Products.jsx
@@ -0,0 +1,19 @@
+export const Products = () => {
+    return (
+        <>
+            homepage;
+            products;
+            contacts;
+            cart;
+            checkout page;
+            thank you page;
+            404 page;
+            frequently asked questions;
+            reviews or cases;
+            search results page;
+            privacy policy and terms of use;
+            account registration page;
+            blog.
+        </>
+    )
+}
---
+++ ecommerce/src/pages/ThankYou.jsx
@@ -0,0 +1,5 @@
+export const ThankYou = () => {
+    return (
+        <></>
+    )
+}

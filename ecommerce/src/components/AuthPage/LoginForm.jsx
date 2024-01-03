f8e1dd36e6c8cb0eeec5e3e99357c9e291dc46d3
Md Hasib Askari <mdhsbaskari@gmail.com>
2024-01-04 02:14:02 +0600

    Module 19 Assignment

--- ecommerce/src/components/AuthPage/LoginForm.jsx
+++ ecommerce/src/components/AuthPage/LoginForm.jsx
@@ -2,7 +2,7 @@ import { InputForm } from "../Checkout/InputForm.jsx";
 
 export const LoginForm = ({ title }) => {
   return (
-    <form className="hidden space-y-4 md:space-y-6" action="#" id="login">
+    <form className="space-y-4 md:space-y-6" action="#" id="login">
       <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
         {title}
       </h1>
--- ecommerce/src/pages/AuthPage.jsx
+++ ecommerce/src/pages/AuthPage.jsx
@@ -18,27 +18,27 @@ export const AuthPage = () => {
   return (
     <>
       <Header />
-      <div className="pt-20 text-center text-2xl mb-10">
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-          onClick={() => changeForm("login")}
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Login
-          </span>
-        </button>
-        <span className="text-gray-700">| </span>
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
-          onClick={() => changeForm("signup")}
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Register
-          </span>
-        </button>
-      </div>
-      <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <section className="py-20 bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+          <div className="text-center text-2xl mb-10">
+            <button
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+              onClick={() => changeForm("login")}
+            >
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Login
+                  </span>
+            </button>
+            <span className="text-gray-700">| </span>
+            <button
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
+              onClick={() => changeForm("signup")}
+            >
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Register
+                  </span>
+            </button>
+          </div>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <LoginForm title="Login" />
